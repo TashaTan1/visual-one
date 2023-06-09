477a55e741a860be917fc7abce3a45250bb6481a
TashaTan1 <136006359+TashaTan1@users.noreply.github.com>
2023-06-09 20:11:04 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -58,7 +58,7 @@ function draw() {
     button.position(200, height/2);
     button.mousePressed(function goToAnotherPage() {
       window.location.href =
-        "https://editor.p5js.org/natashatan/sketches/DVFCuAp9T";
+        "https://tashatan1.github.io/visual-one/";
     });
     button = createButton("Let's Breath");
     button.position(425, height/2);
